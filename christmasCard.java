0e8b9565caf484e59942dff807b6e967a576ad34
Cecelia Hahn <ccrhahn17@gmail.com>
2024-12-25 17:03:58 -0600

    Create christmasCard.java

---
+++ christmasCard.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    //quizmois card
+	    System.out.println("          &&");
+		System.out.println("          /\\");
+		System.out.println("         /  \\");
+		System.out.println("        /  + \\        MERRY");
+		System.out.println("       / +    \\     CHRISTMAS!");
+		System.out.println("      /        \\");
+		System.out.println("     / +      + \\");
+		System.out.println("     ------------");
+		System.out.println("         |  |");
+		System.out.println("          --");
+	}
+}
